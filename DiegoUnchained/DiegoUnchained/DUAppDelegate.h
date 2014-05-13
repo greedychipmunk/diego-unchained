cc1c2aacc1722426d59131e2f023af06e179bae6
Jabari Bell <jabari.bell@pxlflu.net>
2014-05-13 15:16:28 -0400

    initial setup for project

---
+++ DiegoUnchained/DiegoUnchained/DUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DUAppDelegate.h
+//  DiegoUnchained
+//
+//  Created by Jabari Bell on 5/13/14.
+//  Copyright (c) 2014 Antelope Smash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiegoUnchained/DiegoUnchained/DUViewController.m
@@ -0,0 +1,51 @@
+//
+//  DUViewController.m
+//  DiegoUnchained
+//
+//  Created by Jabari Bell on 5/13/14.
+//  Copyright (c) 2014 Antelope Smash. All rights reserved.
+//
+
+#import "DUViewController.h"
+#import "DUMyScene.h"
+
+@implementation DUViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [DUMyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
