aec9f7c4cee6731e1316bf391755caeca0e1dab6
Jabari Bell <jabari.bell@pxlflu.net>
2014-05-14 23:30:23 -0400

    added base parallax sprite

---
+++ DiegoUnchained/DiegoUnchained/classes/sprite/DUParallaxSprite.h
@@ -0,0 +1,13 @@
+//
+//  DUParallaxSprite.h
+//  DiegoUnchained
+//
+//  Created by Jabari on 5/14/14.
+//  Copyright (c) 2014 Antelope Smash. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface DUParallaxSprite : SKNode
+
+@end
