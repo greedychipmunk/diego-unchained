33b5cb942148f241ef2ce4cd04499f88b35550de
Dawson Blackhouse <db276a@att.com>
2014-05-19 17:07:52 -0400

    adding ground and diego jumping

--- DiegoUnchained/DiegoUnchained/classes/scene/DUTestLevelScene.m
+++ DiegoUnchained/DiegoUnchained/classes/scene/DUTestLevelScene.m
@@ -9,17 +9,62 @@
 #import "DUTestLevelScene.h"
 #import "DUDiegoSprite.h"
 
-@implementation DUTestLevelScene
+@implementation DUTestLevelScene {
+    DUDiegoSprite *_diego;
+    SKSpriteNode *_ground;
+    SKNode *_cieling;
+    
+    BOOL _diegoJumping;
+}
 
 - (instancetype)initWithSize:(CGSize)size
 {
     self = [super initWithSize:size];
     if (self) {
         self.backgroundColor = [UIColor blueColor];
-        DUDiegoSprite *diego = [[DUDiegoSprite alloc] initAtPosition:CGPointMake(CGRectGetMidX(self.frame) - 300, CGRectGetMidY(self.frame) - 200)];
-        [self addChild:diego atWorldLayer:DUWorldLayerMidGround];
+        self.physicsWorld.gravity = CGVectorMake(0, -3.0);
+        
+        _diego = [[DUDiegoSprite alloc] initAtPosition:CGPointMake(CGRectGetMidX(self.frame) - 300, CGRectGetMidY(self.frame) - 160)];
+        _diego.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_diego.frame.size];
+        [self addChild:_diego atWorldLayer:DUWorldLayerMidGround];
+        
+        /*
+         * TODO: add cieling to keep diego from flying away
+        _cieling = [[SKNode alloc] init];
+        _cieling.position = CGPointMake(100, 0);
+        _cieling.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(size.width, 100)];
+        [self addChild:_cieling atWorldLayer:DUWorldLayerMidGround];
+        */
+        
+        _ground = [[SKSpriteNode alloc] initWithImageNamed:@"ground.png"];
+        _ground.name = @"ground";
+        _ground.anchorPoint = CGPointZero;
+        _ground.position = CGPointMake(0, _ground.frame.size.height + 40);
+        
+        _ground.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_ground.frame.size];
+        _ground.physicsBody.dynamic = NO;
+        [self addChild:_ground atWorldLayer:DUWorldLayerMidGround];
+        
+        _diegoJumping = NO;
     }
     return self;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
+    _diegoJumping = YES;
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch ends */
+    _diegoJumping = NO;
+}
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+    if (_diegoJumping) {
+        [_diego.physicsBody applyForce:CGVectorMake(0, 300)];
+    }
 }
 
 @end
